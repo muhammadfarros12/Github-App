615ee5af81ef9f3e626745594aa8c4cd7d30165f
HariAgus <haryagus97@gmail.com>
2021-10-23 21:05:07 +0700

    Fix search user and get data from API

--- app/src/main/java/com/farroos/githubapp/api/RetrofitClient.kt
+++ app/src/main/java/com/farroos/githubapp/api/RetrofitClient.kt
@@ -1,15 +1,34 @@
 package com.farroos.githubapp.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "https://api.github.com/"
+    private const val API_KEY = "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"
+
+    // Code dibawah digunakan untuk menghadle network
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val origin = chain.request()
+                val requestBuilder = origin.newBuilder()
+                    .header("Authorization", API_KEY) // Agar tidak ada penggulangan code di interface Api
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .build()
+    }
 
     // retrofit builder
     var retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+//        .client(client) Untuk menggunakan cukup gunakan .client()
         .build()
 
     val apiInstance = retrofit.create(Api::class.java)
